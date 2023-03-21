7b646d1f1ed23dc8cd9ef141c8ddd26953980857
indicate0 <indicate0@gmail.com>
2023-03-21 06:32:04 +0530

    angular-learning-started

---
+++ angular/typescript-project/src/index.js
@@ -0,0 +1,11 @@
+var username = "John";
+// document.write("Hello! "+username)
+// variables
+function Demo() {
+    var x = 10;
+    if (x == 10) {
+        var y = 20;
+    }
+    console.log("x=" + x + "\ny=" + y);
+}
+Demo();
